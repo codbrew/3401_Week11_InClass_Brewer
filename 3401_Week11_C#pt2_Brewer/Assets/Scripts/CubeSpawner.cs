e7ff5b8be033f31090ce6221a0544e7ba2b53cfc
Cody Brewer <cb535114@scc-mmlab-15.local>
2019-03-26 17:27:18 -0400

    Uploaded Entire Project

---
+++ 3401_Week11_C#pt2_Brewer/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject[] cubePool;
+    public int cubePoolSize = 100;
+
+    public GameObject cubePrefab;
+    public Text cubeCountText;
+    public int cubesPerFrame = 10;
+
+    int _currentCubeCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        SpawnCubePool();
+
+
+
+    }
+    void SpawnCubePool()
+    {
+        cubePool = new GameObject[cubePoolSize];
+        for (int i = 0; i < cubePoolSize; ++i)
+        {
+            GameObject cubeInstance = Instantiate(cubePrefab, transform.position, Quaternion.identity);
+            cubePool[i] = cubeInstance;
+            //makes cube inactive. Can be turned off and on
+            cubeInstance.SetActive(false);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < cubePool.Length; i++)
+        {
+            if (!cubePool[i].activeSelf)
+            {
+                cubePool[i].SetActive(true);
+                cubePool[i].transform.position = transform.position;
+
+                break;
+            }
+        }
+        //for (int i = 0; i < cubesPerFrame; i++)
+        //{
+        //    Instantiate(cubePrefab, transform.position, Quaternion.identity);
+        //    _currentCubeCount += 1;
+
+        //}
+
+        //cubeCountText.text = _currentCubeCount.ToString();
+
+    }
+}
---
+++ 3401_Week11_C#pt2_Brewer/Assets/Scripts/cubeInstance.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeInstance : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float countdown = 5f;
+    float _currentCountdown = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        _currentCountdown = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentCountdown = Addlife ();
+        if (_currentCountdown > countdown)
+        {
+            //resets speed of cube when going inactive
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+            gameObject.SetActive(false);
+        }
+    }
+
+    float Addlife()
+    {
+        _currentCountdown += Time.deltaTime;
+
+        return _currentCountdown;
+    }
+
+}
